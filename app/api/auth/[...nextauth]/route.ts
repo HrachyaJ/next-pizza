b9e185cefd81bc086538d8031d20d9e40004c103
Hrachya <hrachjgshow@gmail.com>
2025-08-01 14:02:39 +0400

    params nextjs 15 update

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,19 @@
+import { NextRequest } from "next/server";
 import NextAuth from "next-auth";
 import { authOptions } from "@/constants/auth-options";
 
 const handler = NextAuth(authOptions);
 
-export { handler as GET, handler as POST };
+export async function GET(
+  request: NextRequest,
+  context: { params: Promise<any> }
+) {
+  return handler(request, context);
+}
+
+export async function POST(
+  request: NextRequest,
+  context: { params: Promise<any> }
+) {
+  return handler(request, context);
+}
