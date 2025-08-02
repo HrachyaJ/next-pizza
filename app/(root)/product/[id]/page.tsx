44e1ef3db194fdec6a39330ad7b28326d4566fcd
Hrachya <hrachjgshow@gmail.com>
2025-08-02 11:24:59 +0400

    deploying error fixed

--- app/(root)/product/[id]/page.tsx
+++ app/(root)/product/[id]/page.tsx
@@ -5,9 +5,9 @@ import { notFound } from "next/navigation";
 export default async function ProductPage({
   params,
 }: {
-  params: { id: string };
+  params: Promise<{ id: string }>;
 }) {
-  const { id } = params;
+  const { id } = await params;
 
   const product = await prisma.product.findFirst({
     where: { id: Number(id) },
